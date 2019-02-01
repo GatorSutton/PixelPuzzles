de945888a6338c4241596d29e6877ac3f22930cb
GatorSutton <garrettsuttonee@gmail.com>
2019-02-01 18:01:08 -0500

    graphics for whackamole
    alien invasion / mole game

---
+++ PixelPuzzles/Assets/alienController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alienController : MonoBehaviour {
+
+    //follows the path to earth
+    //orbits earth if not destroyed while following first path
+    //has a tile associated with itself
+    //falls out of sight when destroyed
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
